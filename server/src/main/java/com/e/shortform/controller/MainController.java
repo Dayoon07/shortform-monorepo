7bd017124b4db9650c3be98916ba28216052c7ed
Dayoon07 <gangd0642@gmail.com>
2025-07-21 19:02:15 +0900

    커밋

---
+++ server/src/main/java/com/e/shortform/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.e.shortform.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
