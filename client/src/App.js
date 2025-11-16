a8c7e241307ff8638bf6c8224c62d81bd64edce2
Dayoon07 <gangd0642@gmail.com>
2025-11-16 20:53:35 +0900

    fix: 브라우저 라우터의 basename을 환경 변수에서 고정된 경로로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ import "./App.css";
 export default function App() {
     return (
         <>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter basename="/shortform-client">
                 <UserProvider>
                     <div>
                         <AppBar />
