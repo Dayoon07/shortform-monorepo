15d5270f7c1dbb9d8e876a376bf97ec72b43a92e
Dayoon <gangd0642@gmail.com>
2025-11-26 09:50:18 +0900

    .

---
+++ server/src/main/java/com/e/shortform/config/oauth/OAuth2SuccessHandler.java
@@ -0,0 +1,33 @@
+package com.e.shortform.config.oauth;
+
+import com.e.shortform.config.JwtUtil;
+import com.e.shortform.domain.user.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest req,
+                                        HttpServletResponse res,
+                                        Authentication auth) throws IOException {
+
+        CustomOAuth2User principal = (CustomOAuth2User) auth.getPrincipal();
+        UserEntity user = principal.getUser();
+
+        String token = jwtUtil.generateToken(user);
+
+        String redirectUrl = "http://localhost:3000/social-login?token=" + token;
+
+        getRedirectStrategy().sendRedirect(req, res, redirectUrl);
+    }
+}
